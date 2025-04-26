029196416956005cc24af13bd6f1016f5931c9ac
Aman Varshney <amanvarshney.work@gmail.com>
2025-04-26 15:57:04 +0530

    initial commit

---
+++ sponsorkit.config.js
@@ -0,0 +1,10 @@
+import { defineConfig, tierPresets } from 'sponsorkit'
+
+
+export default defineConfig({
+  github: {
+    login: 'amanvarshney01',
+    type: 'user',
+  },
+  renderer: 'circles',
+})
